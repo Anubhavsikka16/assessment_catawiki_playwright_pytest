17d07e347d78b6da04ff659b282498553ad420f7
anubhavsikka16 <anubhavsikka@gmail.com>
2026-05-06 14:46:03 +0530

    adding correct path directory to log files

--- utils/config_reader.py
+++ utils/config_reader.py
@@ -3,5 +3,6 @@ import yaml
 def load_config():
     with open("configs/config.yaml") as f:
         return yaml.safe_load(f)
+    config["headless"] = os.getenv("HEADLESS", "true").lower() == "true"
 
 config = load_config()
