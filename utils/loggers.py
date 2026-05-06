01a3a7ac6d4fc12abcea613063db8859827fbe9c
anubhavsikka16 <anubhavsikka@gmail.com>
2026-05-06 14:52:50 +0530

    Addinglogger file

--- utils/loggers.py
+++ utils/loggers.py
@@ -1,20 +1,45 @@
 import logging
+import os
 import time
 
 
-class Logger():
+class Logger:
 
-    def __init__(self, logger, file_level=logging.INFO):
-        self.logger = logging.getLogger(logger)
+    def __init__(self, logger_name, file_level=logging.INFO):
+
+        # Create logger
+        self.logger = logging.getLogger(logger_name)
         self.logger.setLevel(logging.DEBUG)
 
-        fmt = logging.Formatter('%(asctime)s - %(filename)s:[%(lineno)s] - [%(levelname)s] - %(message)s')
+        # Prevent duplicate logs
+        if self.logger.hasHandlers():
+            self.logger.handlers.clear()
 
+        # Create logs directory automatically
+        os.makedirs("logs", exist_ok=True)
+
+        # Log file name
         curr_time = time.strftime("%Y-%m-%d")
-        self.LogFileName = '../Logs/log' + curr_time + '.txt'
-        # "a" to append the logs in same file, "w" to generate new logs and delete old one
-        fh = logging.FileHandler(self.LogFileName, mode="w")
+        self.log_file_name = f"logs/log_{curr_time}.txt"
+
+        # Formatter
+        fmt = logging.Formatter(
+            '%(asctime)s - %(filename)s:[%(lineno)s] - [%(levelname)s] - %(message)s'
+        )
+
+        # File handler
+        fh = logging.FileHandler(self.log_file_name, mode="a")
         fh.setFormatter(fmt)
         fh.setLevel(file_level)
-        self.logger.addHandler(fh)
 
+        # Console handler
+        ch = logging.StreamHandler()
+        ch.setFormatter(fmt)
+        ch.setLevel(file_level)
+
+        # Add handlers
+        self.logger.addHandler(fh)
+        self.logger.addHandler(ch)
+
+    def get_logger(self):
+        return self.logger
